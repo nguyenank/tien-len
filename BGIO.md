3842594f493c447877eabcbff2392a30e3f3512e
An <anknguyenanknguyen@gmail.com>
2020-09-11 12:38:12 -0500

    add tien len stage

---
+++ BGIO.md
@@ -0,0 +1,12 @@
+phases - multiple turns
+stages - within a turn
+
+Round = phase
+
+tien len = stage
+
+in game state, keep track of list of players who are still in and pass turn accordingly
+
+when only one player left in the turn list,
+  - have three different moves: normalPlay, tienLenPlay, and newRoundPlay; only enable second for tien len stage
+  - if they ever take newRoundPlay: reset list of players in the round, and end the tien len stage
--- src/Game.js
+++ src/Game.js
@@ -3,11 +3,12 @@ export const TienLen = {
   setup: setUp,
   moves: {
     playCards: playCards,
-    passTurn: passTurn
+    passTurn: passTurn,
+    tienLenPlay: tienLenPlay,
+    newRoundPlay: newRoundPlay
   },
-  phases: {
-    normalPlay: {start: true,},
-    tienLen: {}
+  stages: {
+    tienLen: {moves: {tienLenPlay, newRoundPlay}}
   }
 
 }
@@ -22,6 +23,16 @@ function playCards(G, ctx) {
   nextTurn(G, ctx);
 }
 
+function tienLenPlay(G, ctx) {
+  nextTurn(G, ctx);
+}
+
+function newRoundPlay(G, ctx) {
+  ctx.events.endStage();
+  G.activePlayers = [0, 1, 2, 3];
+  nextTurn(G, ctx);
+}
+
 function passTurn(G, ctx) {
   G.activePlayers[parseInt(ctx.currentPlayer)] = null;
   nextTurn(G, ctx);
@@ -29,13 +40,14 @@ function passTurn(G, ctx) {
 
 function nextTurn(G, ctx) {
   let currentPlayer = parseInt(ctx.currentPlayer);
-  let playerList = G.activePlayers.slice(currentPlayer+1,4).concat(G.activePlayers.slice(0, currentPlayer+1));
-  console.log(playerList);
-  let nextPlayer = playerList.find(i => i !== null);
-  if (currentPlayer === nextPlayer) {
-    // start Tien Len phase
-  }
-  else {
-    ctx.events.endTurn({next: nextPlayer.toString()});
+  let playerList = G.activePlayers.slice(currentPlayer+1,4)
+                  .concat(G.activePlayers.slice(0, currentPlayer+1));
+  let nextPlayer = playerList.find(i => i !== null).toString();
+
+  ctx.events.endTurn({next: nextPlayer});
+  if (G.activePlayers.filter(x => x !== null).length === 1) {
+    let value = {};
+    value[nextPlayer] = 'tienLen';
+    ctx.events.setActivePlayers({value: value});
   }
 }
