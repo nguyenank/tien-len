72073194576be69e8b1192902600e9cde9003677
An <anknguyenanknguyen@gmail.com>
2020-09-13 14:56:57 -0500

    :ambulance: fix dependency issues

--- src/TienLen.js
+++ src/TienLen.js
@@ -13,7 +13,7 @@ export const TienLen = {
 
 function setUp() {
   return {
-    activePlayers: [0, 1, 2, 3],
+    turnOrder: [0, 1, 2, 3],
   };
 }
 
@@ -27,24 +27,24 @@ export function tienLenPlay(G, ctx) {
 
 export function newRoundPlay(G, ctx) {
   ctx.events.endStage();
-  G.activePlayers = [0, 1, 2, 3];
+  G.turnOrder = [0, 1, 2, 3];
   nextTurn(G, ctx);
 }
 
 export function passTurn(G, ctx) {
-  G.activePlayers[parseInt(ctx.currentPlayer)] = null;
+  G.turnOrder[parseInt(ctx.currentPlayer)] = null;
   nextTurn(G, ctx);
 }
 
 export function nextTurn(G, ctx) {
   let currentPlayer = parseInt(ctx.currentPlayer);
-  let playerList = G.activePlayers
+  let playerList = G.turnOrder
     .slice(currentPlayer + 1, 4)
-    .concat(G.activePlayers.slice(0, currentPlayer + 1));
+    .concat(G.turnOrder.slice(0, currentPlayer + 1));
   let nextPlayer = playerList.find(i => i !== null).toString();
 
   ctx.events.endTurn({ next: nextPlayer });
-  if (G.activePlayers.filter(x => x !== null).length === 1) {
+  if (G.turnOrder.filter(x => x !== null).length === 1) {
     let value = {};
     value[nextPlayer] = "tienLen";
     ctx.events.setActivePlayers({ value: value });
---
+++ src/tests/turnOrder.test.js
@@ -0,0 +1,111 @@
+import { Client } from "boardgame.io/client";
+import { TienLen } from "../TienLen";
+
+describe("Turn order", () => {
+  let client;
+
+  beforeEach(() => {
+    client = Client({
+      game: TienLen,
+    });
+  });
+
+  it("should pass to the next player, wrapping around", () => {
+    // pass turn along
+    client.moves.playCards();
+
+    let { ctx } = client.store.getState();
+
+    expect(ctx.currentPlayer).toEqual("1");
+
+    client.moves.playCards();
+    ctx = client.store.getState()["ctx"];
+    expect(ctx.currentPlayer).toEqual("2");
+
+    client.moves.playCards();
+    ctx = client.store.getState()["ctx"];
+    expect(ctx.currentPlayer).toEqual("3");
+
+    client.moves.playCards();
+    ctx = client.store.getState()["ctx"];
+    let G = client.store.getState()["G"];
+    expect(ctx.currentPlayer).toEqual("0");
+    expect(G.turnOrder).toEqual([0, 1, 2, 3]);
+  });
+
+  describe("skips", () => {
+    let G;
+
+    beforeEach(() => {
+      G = client.store.getState()["G"];
+      G.turnOrder = [0, null, 2, 3];
+    });
+
+    it("should skip any players who have passed", () => {
+      client.moves.playCards();
+
+      let { ctx } = client.store.getState();
+
+      expect(ctx.currentPlayer).toEqual("2");
+
+      client.moves.playCards();
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("3");
+
+      client.moves.playCards(); // player 3 passes
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("0");
+
+      client.moves.playCards();
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("2");
+      G = client.store.getState()["G"];
+      expect(G.turnOrder).toEqual([0, null, 2, 3]);
+    });
+
+    it("should remove players who pass from the turn order", () => {
+      client.moves.playCards();
+
+      let { ctx } = client.store.getState();
+
+      expect(ctx.currentPlayer).toEqual("2");
+
+      client.moves.playCards();
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("3");
+
+      client.moves.passTurn(); // player 3 passes
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("0");
+
+      client.moves.playCards();
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("2");
+
+      client.moves.playCards();
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("0");
+      G = client.store.getState()["G"];
+      expect(G.turnOrder).toEqual([0, null, 2, null]);
+    });
+  });
+
+  describe("tien len", () => {
+    let G;
+    let ctx;
+
+    beforeEach(() => {
+      G = client.store.getState()["G"];
+      ctx = client.store.getState()["ctx"];
+      ctx.currentPlayer = "1";
+      G.turnOrder = [null, 1, 2, null];
+    });
+
+    it("should start whenever 1 player is left in turn order", () => {
+      client.moves.passTurn();
+      ctx = client.store.getState()["ctx"];
+      expect(ctx.currentPlayer).toEqual("2");
+      expect(ctx.activePlayers).toEqual({ "2": "tienLen" });
+    });
+  });
+});
