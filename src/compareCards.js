a7876533c66a2e2deaabc9b0730885842fc6020c
An <anknguyenanknguyen@gmail.com>
2020-09-13 16:21:44 -0500

    :sparkles: initial implementation of validating combinations

--- src/compareCards.js
+++ src/compareCards.js
@@ -1,5 +1,7 @@
 import { Constants } from "./constants";
 
+const _ = require("lodash");
+
 export function compareCards(card1, card2) {
   let SUITS = Constants.SUITS;
   let RANKS = Constants.RANKS;
@@ -23,3 +25,36 @@ export function compareCards(card1, card2) {
   }
   return 0;
 }
+
+export function validCombination(cards) {
+  let RANKS = Constants.RANKS;
+
+  if (cards.length === 1) {
+    // single
+    return true;
+  } else if (cards.length === 2 && cards[0].rank === cards[1].rank) {
+    // pair
+    return true;
+  } else if (
+    cards.length === 3 &&
+    cards[0].rank === cards[1].rank &&
+    cards[0].rank === cards[2].rank
+  ) {
+    return true;
+  } else if (cards.length >= 3) {
+    // straight
+    if (cards.some(card => card.rank === 2)) {
+      return false;
+    }
+
+    let sortedCards = cards.sort(compareCards);
+    let ranks = cards.map(card => card.rank);
+    let firstIndex = RANKS.indexOf(ranks[0]);
+    if (_.isEqual(RANKS.slice(firstIndex, firstIndex + ranks.length), ranks)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function validChop(cards) {}
--- src/tests/compareCards.test.js
+++ src/tests/compareCards.test.js
@@ -2,16 +2,11 @@ import { Client } from "boardgame.io/client";
 import { compareCards } from "../compareCards";
 
 describe("compareCards", () => {
-  let nineClubs;
-  let nineDiamonds;
-  let tenClubs;
-  let tenDiamonds;
-  beforeEach(() => {
-    nineClubs = { suit: "C", rank: "9" };
-    nineDiamonds = { suit: "D", rank: "9" };
-    tenClubs = { suit: "C", rank: "T" };
-    tenDiamonds = { suit: "D", rank: "T" };
-  });
+  let nineClubs = { suit: "C", rank: "9" };
+  let nineDiamonds = { suit: "D", rank: "9" };
+  let tenClubs = { suit: "C", rank: "T" };
+  let tenDiamonds = { suit: "D", rank: "T" };
+
   it("should first compare by rank", () => {
     expect(compareCards(nineClubs, tenClubs)).toBe(-1);
     expect(compareCards(nineClubs, tenDiamonds)).toBe(-1);
