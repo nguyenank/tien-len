2eefb47957a0f7cc99107c080f46cb39d025bbbc
An <anknguyenanknguyen@gmail.com>
2020-09-19 15:08:17 -0500

    :sparkles: implement validChop

--- src/compareCards.js
+++ src/compareCards.js
@@ -3,6 +3,10 @@ import { Constants } from "./constants";
 const _ = require("lodash");
 
 export function compareCards(card1, card2) {
+  /*
+    For two cards, returns 1 if if card1 is higher than card2,
+    0 if they are the same, and -1 if card1 is lower than card2
+  */
   let SUITS = Constants.SUITS;
   let RANKS = Constants.RANKS;
 
@@ -27,6 +31,10 @@ export function compareCards(card1, card2) {
 }
 
 export function validCombination(cards) {
+  /*
+    for an array of cards, returns true if
+    the combination is a normal combination
+  */
   let RANKS = Constants.RANKS;
 
   if (cards.length === 1) {
@@ -63,4 +71,33 @@ export function validCombination(cards) {
   return false;
 }
 
-export function validChop(cards) {}
+export function validChop(cards) {
+  // cannot be 2's in a chop
+  if (cards.some(card => card.rank === 2)) {
+    return false;
+  }
+  // four of a kind
+  if (cards.length === 4) {
+    return cards.every(card => card.rank === cards[0].rank);
+  }
+  // three or four pair
+  if (cards.length === 6 || cards.length === 8) {
+    let groupedCards = _.groupBy(cards, "rank");
+    // only pairs
+    let onlyPairs = _.every(groupedCards, card => {
+      return card.length === 2;
+    });
+    // consecutive
+    let RANKS = Constants.RANKS;
+    cards.sort(compareCards);
+    let ranks = _.uniq(cards.map(card => card.rank));
+    let firstIndex = RANKS.indexOf(ranks[0]);
+    let consecutive = _.isEqual(
+      RANKS.slice(firstIndex, firstIndex + ranks.length),
+      ranks
+    );
+
+    return onlyPairs && consecutive;
+  }
+  return false;
+}
--- src/tests/compareCards.test.js
+++ src/tests/compareCards.test.js
@@ -1,5 +1,5 @@
 import { Client } from "boardgame.io/client";
-import { compareCards } from "../compareCards";
+import { compareCards, validCombination, validChop } from "../compareCards";
 
 describe("compareCards", () => {
   let nineClubs = { suit: "C", rank: "9" };
@@ -25,5 +25,162 @@ describe("compareCards", () => {
     expect(compareCards(nineClubs, nineClubs)).toBe(0);
 
     expect(compareCards(tenDiamonds, tenDiamonds)).toBe(0);
+  });
+});
+
+describe("validCombination", () => {
+  let sevenSpades = { suit: "S", rank: "7" };
+  let eightDiamonds = { suit: "D", rank: "8" };
+  let nineClubs = { suit: "C", rank: "9" };
+  let nineDiamonds = { suit: "D", rank: "9" };
+  let nineHearts = { suit: "H", rank: "9" };
+  let tenClubs = { suit: "C", rank: "T" };
+  let tenDiamonds = { suit: "D", rank: "T" };
+  let tenHearts = { suit: "H", rank: "T" };
+
+  let threeClubs = { suit: "C", rank: "3" };
+  let fourSpades = { suit: "S", rank: "4" };
+  let twoHearts = { suit: "H", rank: "2" };
+  let aceDiamonds = { suit: "D", rank: "A" };
+  let kingDiamonds = { suit: "D", rank: "K" };
+
+  it("should allow singles", () => {
+    expect(validCombination([nineClubs])).toBe(true);
+    expect(validCombination([tenDiamonds])).toBe(true);
+  });
+
+  it("should allow pairs", () => {
+    expect(validCombination([nineClubs, nineDiamonds])).toBe(true);
+
+    expect(validCombination([tenDiamonds, tenClubs])).toBe(true);
+  });
+
+  it("should allow triples", () => {
+    expect(validCombination([nineClubs, nineDiamonds, nineHearts])).toBe(true);
+
+    expect(validCombination([tenDiamonds, tenClubs, tenHearts])).toBe(true);
+  });
+
+  it("should allow straights of at least length 3", () => {
+    expect(validCombination([nineClubs, tenDiamonds, eightDiamonds])).toBe(
+      true
+    );
+
+    expect(validCombination([twoHearts, fourSpades, threeClubs])).toBe(false);
+  });
+
+  it("should not allow straights including a 2", () => {
+    expect(validCombination([twoHearts, kingDiamonds, aceDiamonds])).toBe(
+      false
+    );
+  });
+
+  it("should not allow anything not specified above", () => {
+    expect(validCombination([])).toBe(false);
+
+    expect(validCombination([nineClubs, tenDiamonds])).toBe(false);
+
+    expect(validCombination([nineDiamonds, tenClubs, tenHearts])).toBe(false);
+
+    expect(validCombination([nineDiamonds, sevenSpades, tenHearts])).toBe(
+      false
+    );
+  });
+});
+
+describe("validChop", () => {
+  let sevenSpades = { suit: "S", rank: "7" };
+  let sevenHearts = { suit: "H", rank: "7" };
+  let eightSpades = { suit: "S", rank: "8" };
+  let eightDiamonds = { suit: "D", rank: "8" };
+  let nineSpades = { suit: "S", rank: "9" };
+  let nineClubs = { suit: "C", rank: "9" };
+  let nineDiamonds = { suit: "D", rank: "9" };
+  let nineHearts = { suit: "H", rank: "9" };
+  let tenSpades = { suit: "S", rank: "T" };
+  let tenClubs = { suit: "C", rank: "T" };
+  let tenDiamonds = { suit: "D", rank: "T" };
+  let tenHearts = { suit: "H", rank: "T" };
+
+  let threeClubs = { suit: "C", rank: "3" };
+  let fourSpades = { suit: "S", rank: "4" };
+  let twoHearts = { suit: "H", rank: "2" };
+  let threeSpades = { suit: "S", rank: "3" };
+  let fourHearts = { suit: "H", rank: "4" };
+  let twoClubs = { suit: "C", rank: "2" };
+
+  it("should allow three pairs", () => {
+    expect(
+      validChop([
+        nineClubs,
+        nineSpades,
+        tenHearts,
+        tenDiamonds,
+        eightSpades,
+        eightDiamonds,
+      ])
+    ).toBe(true);
+  });
+
+  it("should allow four of a kind", () => {
+    expect(validChop([nineClubs, nineDiamonds, nineSpades, nineHearts])).toBe(
+      true
+    );
+    expect(validChop([tenDiamonds, tenClubs, tenSpades, tenHearts])).toBe(true);
+  });
+
+  it("should allow four pairs", () => {
+    expect(
+      validChop([
+        nineClubs,
+        nineSpades,
+        tenHearts,
+        tenDiamonds,
+        eightSpades,
+        eightDiamonds,
+        sevenSpades,
+        sevenHearts,
+      ])
+    ).toBe(true);
+  });
+
+  it("should not allow anything not specified above", () => {
+    expect(validChop([])).toBe(false);
+
+    // not pairs
+    expect(
+      validChop([
+        nineClubs,
+        nineSpades,
+        tenHearts,
+        tenDiamonds,
+        nineHearts,
+        eightDiamonds,
+      ])
+    ).toBe(false);
+
+    // not consecutive
+    expect(
+      validChop([
+        nineClubs,
+        nineSpades,
+        tenHearts,
+        tenDiamonds,
+        sevenSpades,
+        sevenHearts,
+      ])
+    ).toBe(false);
+
+    // no twos
+    expect(
+      validChop([
+        threeClubs,
+        threeSpades,
+        fourSpades,
+        twoClubs,
+        fourHearts,
+        twoHearts,
+      ])
+    ).toBe(false);
   });
 });
