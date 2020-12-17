038a9835ce51ce8d262859780e8450e0ef04772a
An <anknguyenanknguyen@gmail.com>
2020-12-16 21:50:25 -0600

    :rocket: switch to production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const GAME_SERVER_PORT = 8000;
 export const GAME_SERVER_URL = `http://localhost:${GAME_SERVER_PORT}`;
 export const WEB_SERVER_URL = "http://localhost:8000";
-export const APP_PRODUCTION = false;
+export const APP_PRODUCTION = true;
